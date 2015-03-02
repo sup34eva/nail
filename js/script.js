ca6be858babc1ba3b6b3fb84555e13c6329cdf0d
naldir <th.gonet@gmail.com>
2015-03-02 13:19:12 +0100

    pyramid

--- js/script.js
+++ js/script.js
@@ -2,41 +2,104 @@ $(document).ready(function(){
         var canvas = document.getElementById("renderCanvas");
         var engine = new BABYLON.Engine(canvas, true);
         var fpsLabel = document.getElementById("fpsLabel");
+    
+        CreateLine = function (name, width, scene) {
+            var line = BABYLON.Mesh.CreateLines(name, [
+                new BABYLON.Vector3(-(width), 0, 0),
+                new BABYLON.Vector3(width, 0, 0)
+            ], scene);
+            
+            return line;
+        }
+    
+        BABYLON.Mesh.CreatePyramid4 = function (name, baseSize, height, scene, updatable) {
+          var pyramid = new BABYLON.Mesh(name, scene);
 
-        var createScene = function () {
-        
-            // This creates a basic Babylon Scene object (non-mesh)
-            var scene = new BABYLON.Scene(engine);
-        
-            // This creates and positions a free camera (non-mesh)
-            var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-        
-            // This targets the camera to scene origin
-            camera.setTarget(BABYLON.Vector3.Zero());
-        
-            // This attaches the camera to the canvas
-            camera.attachControl(canvas, true);
-        
-            // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-        
-            // Default intensity is 1. Let's dim the light a small amount
-            light.intensity = 0.7;
-        
-            // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-            var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-        
-            // Move the sphere upward 1/2 its height
-            sphere.position.y = 1;
-        
-            // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-            var ground = BABYLON.Mesh.CreateGround("ground1", 10, 10, 2, scene);
-        
-            return scene;
-        
-        };
-        
-        var scene = createScene();
+          // Adding faces
+          var positions = [
+            // Front face
+            0,  height/2,  0,
+            baseSize/2, -height/2, baseSize/2,
+            -baseSize/2, -height/2, baseSize/2,
+
+            // Right face
+            0, height/2, 0,
+            baseSize/2, -height/2, -baseSize/2,
+            baseSize/2, -height/2, baseSize/2,
+
+            // Back face
+            0, height/2,  0,
+            -baseSize/2, -height/2, -baseSize/2,
+            baseSize/2, -height/2, -baseSize/2,
+
+            // Left face
+            0, height/2,  0,
+            -baseSize/2, -height/2, baseSize/2,
+            -baseSize/2, -height/2, -baseSize/2,
+
+            // Bottom face
+            -baseSize/2, -height/2, baseSize/2,
+            baseSize/2, -height/2, baseSize/2,
+            baseSize/2, -height/2, -baseSize/2,
+            -baseSize/2, -height/2, -baseSize/2
+          ];
+
+          var normals = [
+            height, baseSize/2, 0,
+            height, baseSize/2, 0,
+            height, baseSize/2, 0,
+
+            0, baseSize/2, height,
+            0, baseSize/2, height,
+            0, baseSize/2, height,
+
+            -height, baseSize/2, 0,
+            -height, baseSize/2, 0,
+            -height, baseSize/2, 0,
+
+            0, baseSize/2, -height,
+            0, baseSize/2, -height,
+            0, baseSize/2, -height,
+
+            0, -1, 0,
+            0, -1, 0,
+            0, -1, 0,
+            0, -1, 0
+          ];
+
+          var indices = [];
+          var uvs = [];
+          var i = 0;
+          while (i < 12) {
+            indices.push(i+0);
+            uvs.push(1.0, 1.0);
+            indices.push(i+1);
+            uvs.push(0.0, 1.0);
+            indices.push(i+2);
+            uvs.push(0.0, 0.0);
+            i = i+3;
+          }
+
+          indices.push(12);
+          indices.push(13);
+          indices.push(14);
+
+          indices.push(12);
+          indices.push(14);
+          indices.push(15);
+
+          uvs.push(1.0, 1.0);
+          uvs.push(0.0, 1.0);
+          uvs.push(0.0, 0.0);
+          uvs.push(1.0, 0.0);
+
+          pyramid.setVerticesData(BABYLON.VertexBuffer.PositionKind, positions, updatable);
+          pyramid.setVerticesData(BABYLON.VertexBuffer.NormalKind, normals, updatable);
+          pyramid.setVerticesData(BABYLON.VertexBuffer.UVKind, uvs, updatable);
+          pyramid.setIndices(indices);
+
+          return pyramid;
+        }
 
         engine.runRenderLoop(function () {
             scene.render();
@@ -47,7 +110,48 @@ $(document).ready(function(){
         window.addEventListener("resize", function () {
             engine.resize();
         });
+    
+        var createScene = function () {
+            var scene = new BABYLON.Scene(engine);
 
+            // Setup a simple environment
+            var light0 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(10, 10, 20), scene);
+            
+            var cube = BABYLON.Mesh.CreateBox("cube", 1.0, scene);
+            var sphere = BABYLON.Mesh.CreateSphere("sphere", 16, 1.0, scene);
+            var cylinder = BABYLON.Mesh.CreateCylinder("cylinder", 1, 1, 1, 40, 1.0, scene, false);
+            var pyramid = BABYLON.Mesh.CreatePyramid4("pyramid", 1, 1, scene, false);
+            var line = BABYLON.Mesh.CreateLines(name, [
+                new BABYLON.Vector3(-4, 0, 0),
+                new BABYLON.Vector3(2, 0, 0)
+            ], scene);
+            cube.position.x = -4;
+            sphere.position.x = -2;
+            //cylinder.position.x = 0;
+            pyramid.position.x = 2;
+
+            // ArcRotateCamera >> Camera rotating around a 3D point (here Vector zero)
+            // Parameters : name, alpha, beta, radius, target, scene
+            var arcCamera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
+            arcCamera.setPosition(new BABYLON.Vector3(0, 0, 50));
+            arcCamera.target = new BABYLON.Vector3(3, 0, 0);
+
+            // FreeCamera >> You can move around the world with mouse and keyboard (LEFT/RIGHT/UP/DOWN)
+            // Parameters : name, position, scene
+            var freeCamera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 0, 5), scene);
+            freeCamera.rotation = new BABYLON.Vector3(0, Math.PI, 0);
+
+            // TouchCamera >> Move in your world with your touch screen (or with your mouse, by drag/drop)
+            // Parameters : name, position, scene
+            var touchCamera = new BABYLON.TouchCamera("TouchCamera", new BABYLON.Vector3(0, 0, 10), scene);
+            touchCamera.rotation = new BABYLON.Vector3(0, Math.PI, 0);
+
+            //Attach a camera to the scene and the canvas
+            scene.activeCamera = freeCamera;
+            freeCamera.attachControl(canvas, true);
+
+            return scene;
+        };
+    
+        var scene = createScene();
 })
-
-
