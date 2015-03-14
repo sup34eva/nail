367bfaad6add3bdc5238d04bfb5e1359e4e773c0
Sboob <antho10440@hotmail.fr>
2015-03-14 17:01:03 +0100

    Creation et gestion de sphere
    tout est dit, un petit bout de css ambiance darky

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
+/*if (confirm('Pour accéder à ce site vous devez avoir 18 ans ou plus, cliquez sur "OK" si c\'est le cas.')) {
+    alert('Vous allez être redirigé vers le site.');
+}
+
+else {
+    alert("Désolé, vous n'avez pas accès à ce site.");
+}*/
+
 $(document).ready(function(){
         var canvas = document.getElementById("renderCanvas");
         var engine = new BABYLON.Engine(canvas, true);
+
 
         var createScene = function () {
         
             // This creates a basic Babylon Scene object (non-mesh)
             var scene = new BABYLON.Scene(engine);
-        
+            scene.collisionsEnabled = true;
+
             // This creates and positions a free camera (non-mesh)
             var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-        
+            camera.keysUp = [90]; // Touche Z
+            camera.keysDown = [83]; // Touche S
+            camera.keysLeft = [81]; // Touche Q
+            camera.keysRight = [68]; // Touche D;
+            camera.checkCollisions = true;
+
+
             // This targets the camera to scene origin
             camera.setTarget(BABYLON.Vector3.Zero());
         
@@ -17,24 +33,90 @@ $(document).ready(function(){
             camera.attachControl(canvas, true);
         
             // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-        
-            // Default intensity is 1. Let's dim the light a small amount
-            light.intensity = 0.7;
-        
-            // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-            var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-        
-            // Move the sphere upward 1/2 its height
-            sphere.position.y = 1;
+            var light = new BABYLON.PointLight("DirLight", new BABYLON.Vector3(100, 100 , 100), scene);
+            light.diffuse = new BABYLON.Color3(1, 1, 1);
+            light.specular = new BABYLON.Color3(0.6, 0.6, 0.6);
+            light.intensity = 1.5;
         
             // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-            var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-        
+            var ground = BABYLON.Mesh.CreateGround("ground1", 250, 250, 2, scene);
+            ground.material = new BABYLON.StandardMaterial("gMaterial", scene);
+            ground.material.diffuseTexture = new BABYLON.Texture("img/ground.jpg", scene);
+            ground.checkCollisions = true;
+
+            // Création d'une material
+            var sMaterial = new BABYLON.StandardMaterial("skyboxMaterial", scene);
+            sMaterial.backFaceCulling = false;
+            sMaterial.reflectionTexture = new BABYLON.CubeTexture("img/bluesky", scene);
+            sMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+
+
+            // Création d'un cube avec la material adaptée
+            var skybox = BABYLON.Mesh.CreateBox("skybox", 250, scene);
+            skybox.material = sMaterial;
+
+
+
             return scene;
         
         };
+
+//*****************************Creation et gestion de SPHERE*******************************************
+        function Sphere(){
+            this.nom = document.getElementById('name').value;
+            this.sphere = BABYLON.Mesh.CreateSphere(this.nom, 32, 1, scene);
+        }
+
+        var btn_sphere = document.getElementById('sphere');
         
+        btn_sphere.onclick = function() {
+            s = new Sphere();
+            };
+
+        var positiony = document.getElementById('posy');
+
+        positiony.onchange = function() {
+            s.sphere.position.y = positiony.value;
+            };
+
+        var positionx = document.getElementById('posx');
+
+        positionx.onchange = function() {
+            s.sphere.position.x = positionx.value;
+            };
+
+        var positionz = document.getElementById('posz');
+
+        positionz.onchange = function() {
+            s.sphere.position.z = positionz.value;
+            };
+
+        var taillex = document.getElementById('taillex');
+
+        taillex.onchange = function() {
+            s.sphere.scaling.x = taillex.value;
+            };
+
+        var tailley = document.getElementById('tailley');
+
+        tailley.onchange = function() {
+            s.sphere.scaling.y = tailley.value;
+            };
+
+        var taillez = document.getElementById('taillez');
+
+        taillez.onchange = function() {
+            s.sphere.scaling.z = taillez.value;
+            };
+
+        var name = document.getElementById('name');
+
+        name.onchange = function() {
+            s.sphere.name = name.value;
+            };
+
+//*****************************Fin Creation et gestion de SPHERE*******************************************
+
         var scene = createScene();
 
         engine.runRenderLoop(function () {
@@ -45,4 +127,7 @@ $(document).ready(function(){
         window.addEventListener("resize", function () {
             engine.resize();
         });
-})
+
+    })
+
+
