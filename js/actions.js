91646ea065e33951fbc867747387c6beec44c54a
Sboob <antho10440@hotmail.fr>
2015-06-15 21:41:13 +0200

    Commentaire

--- js/actions.js
+++ js/actions.js
@@ -13,23 +13,23 @@ fullscreen.onclick = function() {
   //*********************Ajout objet**********************************************
 var btn_add = document.getElementById('btn_add');
 btn_add.onclick = function() {
-  meshCount++; //index tu tableau d'objets
-  index = meshCount;
+  meshCount++; //index du tableau d'objets
+  index = meshCount; //Index du du boutton courant
 
   world.scenes[world.zone_id].meshTab[meshCount] = new Mesh(); //Instanciation d'un objet
 
   var button = document.createElement("label"); // Cree un bouton
   button.innerHTML = '<input type="radio">' + document.getElementById('name').value; // Met un titre au bouton
-  button.dataset.mesh = index;
+  button.dataset.mesh = index; //Cree un dataset mesh avec l'index du bouton
   button.onclick = function(e) {
-    indexation(this.dataset.mesh);
+    indexation(this.dataset.mesh); //Quand on clique sur le boutton renvoie son index dans la fonction indexation
   };
   ["btn", "btn-success", "nav-justified"].forEach(function(e) {
-    button.classList.add(e)
+    button.classList.add(e) //Ajoute toutes classes au label
   }); // Ajoute des class
   document.getElementById("objets").appendChild(button); // Ajoute le bouton dans la page
 
-  positionx.value = 1;
+  positionx.value = 1; //Donne des valeurs par defaut a l objet
   positiony.value = 1;
   positionz.value = 1;
   taillex.value = 0;
@@ -57,14 +57,14 @@ btn_sup.onclick = function() {
         }
       }
     }
-    meshCount--;
+    meshCount--; //Decremente le compte d'objets
   } else //si objet groupé
   {
     document.querySelector('[data-grp="' + index + '"]').remove(); //Supprime le boutton
-    world.scenes[world.zone_id].grp_tab.splice(index, 1);
+    world.scenes[world.zone_id].grp_tab.splice(index, 1); // Supprime la case dans le tableau
     var nodes = document.getElementById("objets").childNodes;
     for (i = 0; i < nodes.length; i++) {
-      if (nodes[i].dataset.grp > index) { //Pour chaque élément ayant un id < a celui supprimer
+      if (nodes[i].dataset.grp > index) { //Pour chaque élément ayant un id > a celui supprimer
         nodes[i].dataset.grp = nodes[i].dataset.grp - 1; // Leur id décremente de 1
       }
     }
@@ -77,33 +77,33 @@ var btn_ok = document.getElementById('btn_ok_grp');
 btn_ok.onclick = function() { //Fonction groupe
 
   if (document.getElementById("objets").querySelectorAll(":checked").length > 1) {
-    btn_ok.style.display = "none";
+    btn_ok.style.display = "none"; //Fait disparaitre le bouton
     world.scenes[world.zone_id].grp_tab.push({
       name: "Groupe",
-      objects: Array.prototype.slice.call(document.getElementById("objets").querySelectorAll(":checked")).map(function(e) {
+      objects: Array.prototype.slice.call(document.getElementById("objets").querySelectorAll(":checked")).map(function(e) { //Prend tous les objets checked et les mets dans un tableau
         return e.parentNode.dataset.mesh;
       })
     });
     var button = document.createElement("label"); // Cree un bouton
     button.innerHTML = '<input type="radio">' + "Groupe"; // Met un titre au bouton
-    button.setAttribute("name", "Groupe");
+    button.setAttribute("name", "Groupe"); // Donne le nom groupe au boutton
     var grp_id = grp_count;
-    button.dataset.grp = grp_id;
+    button.dataset.grp = grp_id; // cree un dataset grp avec l'index du bouton
     button.onclick = function(e) {
-      grp_index(this.dataset.grp);
+      grp_index(this.dataset.grp); //Quand on clique renvoie l'index du bouton dans la fonction grp_index
     };
     ["btn", "btn-violet", "nav-justified"].forEach(function(e) {
-      button.classList.add(e)
+      button.classList.add(e) // Ajoute toutes les classes css au bouton
     }); // Ajoute des class
     document.getElementById("objets").appendChild(button); // Ajoute le bouton dans la page
-    grp_count++;
+    grp_count++; //Increment le compteur de boutons de groupe
   } else {
-    alert("Selectionnez au moins 2 objets.");
-    btn_ok.style.display = "none";
+    alert("Selectionnez au moins 2 objets."); //Leve une erreur quand on essaie de grouper moins de 2 objets
+    btn_ok.style.display = "none"; // Cache le bouton ok
   }
-  Array.prototype.slice.call(document.getElementById("objets").querySelectorAll("[type=checkbox]")).forEach(function(e) {
-    e.type = "radio";
-    e.checked = false;
+  Array.prototype.slice.call(document.getElementById("objets").querySelectorAll("[type=checkbox]")).forEach(function(e) { //pour tous les elements de type checkbox
+    e.type = "radio"; //La checkbox devient un input type radio
+    e.checked = false; //Elle n'est pas check
   });
 };
 //*********************Modif groupe*********************************************
@@ -156,18 +156,18 @@ grp_poszm.onclick = function() //z moins
   };
 
 var check = document.getElementById('check');
-check.onclick = function() {
+check.onclick = function() { //Fonction qui permet d'agrandir un d'objet de la meme valeur dans toutes les directions
   if ($(".hiden").css("display") == "none") {
-    $(".hiden").css("display", "block");
+    $(".hiden").css("display", "block"); // Fait apparaitre les autres inputs pour les tailles
   } else {
-    $(".hiden").css("display", "none");
+    $(".hiden").css("display", "none"); // Fait disparaitre les autres inputs pour les tailles
   }
 };
-//******************Agrandissement synchro**************************************
+
 var taillex = document.getElementById('taillex');
 taillex.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].scaling.x = taillex.value;
-  if (check.checked) {
+  world.scenes[world.zone_id].meshTab[index].scaling.x = taillex.value; //Change la taille x
+  if (check.checked) { //******************Agrandissement synchro***************
     world.scenes[world.zone_id].meshTab[index].scaling.y = taillex.value;
     world.scenes[world.zone_id].meshTab[index].scaling.z = taillex.value;
   }
@@ -175,82 +175,83 @@ taillex.onchange = function() {
 //*********************Modif meshs**********************************************
 var tailley = document.getElementById('tailley');
 tailley.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].scaling.y = tailley.value;
+  world.scenes[world.zone_id].meshTab[index].scaling.y = tailley.value; //Change la taille y
 };
 
 var taillez = document.getElementById('taillez');
 taillez.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].scaling.z = taillez.value;
+  world.scenes[world.zone_id].meshTab[index].scaling.z = taillez.value; //Change la taille z
 };
 
 var rotx = document.getElementById('rotx');
 rotx.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].rotation.x = rotx.value / 180 * Math.PI;
+  world.scenes[world.zone_id].meshTab[index].rotation.x = rotx.value / 180 * Math.PI; //Converti les radians en degres et fait tourner l'objet sur l'axe x
 };
 
 var roty = document.getElementById('roty');
 roty.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].rotation.y = roty.value / 180 * Math.PI;
+  world.scenes[world.zone_id].meshTab[index].rotation.y = roty.value / 180 * Math.PI; //Converti les radians en degres et fait tourner l'objet sur l'axe y
 };
 
 var rotz = document.getElementById('rotz');
 rotz.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].rotation.z = rotz.value / 180 * Math.PI;
+  world.scenes[world.zone_id].meshTab[index].rotation.z = rotz.value / 180 * Math.PI; //Converti les radians en degres et fait tourner l'objet sur l'axe z
 };
 
 var positiony = document.getElementById('posy');
 positiony.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].position.y = positiony.value;
+  world.scenes[world.zone_id].meshTab[index].position.y = positiony.value; //Change la position y
 };
 
 var positionx = document.getElementById('posx');
 positionx.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].position.x = positionx.value;
+  world.scenes[world.zone_id].meshTab[index].position.x = positionx.value; //Change la position x
 };
 
 var positionz = document.getElementById('posz');
 positionz.onchange = function() {
-  world.scenes[world.zone_id].meshTab[index].position.z = positionz.value;
+  world.scenes[world.zone_id].meshTab[index].position.z = positionz.value; //Change la position z
 };
 
 var camReset = document.getElementById('camReset');
 camReset.onclick = function() {
-  world.scenes[world.zone_id].scene.activeCamera.position = new BABYLON.Vector3(0, 0, 0);
+  world.scenes[world.zone_id].scene.activeCamera.position = new BABYLON.Vector3(0, 0, 0); //Met la camera a 0, 0, 0
 };
 
 var zoneadd = document.getElementById('zone_add');
-zoneadd.onclick = function() {
-  world.zone_count ++;
-  world.zone_id = world.scenes.push({
-    meshTab: [],
-    grp_tab: [],
-    scene: new BABYLON.Scene(engine)
+zoneadd.onclick = function() { //Function pour ajouter une zone
+  world.zone_count++; //Incremente le compteur de zones
+  world.zone_id = world.scenes.push({ //Met le zone id au nombre de case de world.scene
+    meshTab: [], //Cree un nouveau tableau de mesh
+    grp_tab: [], //Cree un nouveau tableau de groupe
+    scene: new BABYLON.Scene(engine) //Cree une nouvelle scene
   }) - 1;
-  world.scenes[world.zone_id].scene.collisionsEnabled = true;
-  var init = new createScene();
+  world.scenes[world.zone_id].scene.collisionsEnabled = true; //Autorise la collision avec les murs
+  var init = new createScene(); //Rempli la scene avec les elements de base
   var button = document.createElement("label"); // Cree un bouton
   button.innerHTML = '<input type="radio">' + world.name + world.zone_id; // Met un titre au bouton
-  button.dataset.zone = world.zone_count;
+  button.dataset.zone = world.zone_count;  //cree un dataset zone avec l'index du bouton
   button.onclick = function(e) {
-    zoneCharge(this.dataset.zone);
+    zoneCharge(this.dataset.zone); // Quand on clique renvoie l'index du bouton dans la fonction zoneCharge
   };
   ["btn", "btn-success", "nav-justified"].forEach(function(e) {
-    button.classList.add(e)
+    button.classList.add(e) // Ajoute toutes les classes au bouton
   }); // Ajoute des class
   document.getElementById("zones").appendChild(button); // Ajoute le bouton dans la page
-  $("#objets").empty();
+  $("#objets").empty(); // Supprime touts les boutons des objets
+  currentzone.innerHTML = 'Zone :' + world.zone_id; //Change l'affichage de la zone courante
 };
 
 var zonesup = document.getElementById('zone_sup');
-zonesup.onclick = function() {
+zonesup.onclick = function() { //Fonction pour supprimer une zone
   document.querySelector('[data-zone="' + world.zone_id + '"]').remove(); //Supprime le boutton
-  world.scenes.splice(world.zone_id, 1);
+  world.scenes.splice(world.zone_id, 1);//Supprime la case du tableau
   var nodes = document.getElementById("zones").childNodes;
   for (i = 0; i < nodes.length; i++) {
     if (nodes[i].dataset && nodes[i].dataset.zone > world.zone_id) { //Pour chaque élément ayant un id < a celui supprimer
       nodes[i].dataset.zone = nodes[i].dataset.zone - 1; // Leur id décremente de 1
     }
   }
-  world.zone_count --;
-  world.zone_id = world.zone_count;
+  world.zone_count--; //Decremente le compteur de zone
+  world.zone_id = world.zone_count; //Met l'id de la zonne a la derniere scene du tableau pour evite d'etre dans une scene qui n'existe plus 
 }
--- js/zones.js
+++ js/zones.js
@@ -4,37 +4,52 @@ var engine = new BABYLON.Engine(canvas, true);
 function Zones() {
   this.scenes = [];
   this.scenes.push({
-    meshTab: [],
-    grp_tab: [],
-    scene: new BABYLON.Scene(engine)
+    meshTab: [], //Tableau qui contient les meshs
+    grp_tab: [], //Tableau qui contient les groupes
+    scene: new BABYLON.Scene(engine) // Nouvelle scene
   })
-  this.zone_id = 0;
+  this.zone_id = 0; //Id de la zone courante
   this.name = "zone ";
-  this.zone_count = 0;
+  this.zone_count = 0; //Compteur de zone
 }
 
-var world = new Zones();
-world.scenes[world.zone_id].scene.collisionsEnabled = true;
+var world = new Zones(); //Instanciation de la classe Zones
+world.scenes[world.zone_id].scene.collisionsEnabled = true; // Met les colisions avec les murs a true
 
 function zoneCharge(x) {
-  world.zone_id = x;
-
+  world.zone_id = x;//Quand on clique sur un bouton de zone renvoie son index
+  currentzone.innerHTML = 'Zone :' + world.zone_id;
   //supprime les boutons
   $("#objets").empty();
 
   meshCount = 0;
-  world.scenes[world.zone_id].meshTab.forEach(function(e) {
-    //ajout du bouton dans l'interface
+  world.scenes[world.zone_id].meshTab.forEach(function(e) {//Pour chaque objets contenu dans le meshTab
     var button = document.createElement("label"); // Cree un bouton
     button.innerHTML = '<input type="radio">' + e.name; // Met un titre au bouton
-    button.setAttribute("id", meshCount); // L'id sera l'index du tableau
-    button.setAttribute("onClick", "indexation(parseInt(this.id))"); // Donne la function qui gere quel bouton est cliqué
+    button.dataset.mesh = meshCount; //Cree un dataset mesh avec l'index du bouton
+    button.onclick = function(e) {
+      indexation(this.dataset.mesh); //Quand on clique sur le boutton renvoie son index dans la fonction indexation
+    };
     ["btn", "btn-success", "nav-justified"].forEach(function(e) {
-      button.classList.add(e)
+      button.classList.add(e) //Ajoute toutes les classes au bouton
+    });
+    document.getElementById("objets").appendChild(button); // Ajoute le bouton dans la page
+    meshCount++; //Incremente le nombre d'objets
+  });
+
+  grp_count = 0;
+  world.scenes[world.zone_id].grp_tab.forEach(function(e) {//Pour chaque objets contenu dans le meshTab
+    var button = document.createElement("label"); // Cree un bouton
+    button.innerHTML = '<input type="radio">' + e.name; // Met un titre au bouton
+    var grp_id = grp_count;
+    button.dataset.grp = grp_id; // cree un dataset grp avec l'index du bouton
+    button.onclick = function(e) {
+      grp_index(this.dataset.grp); //Quand on clique renvoie l'index du bouton dans la fonction grp_index
+    };
+    ["btn", "btn-violet", "nav-justified"].forEach(function(e) {
+      button.classList.add(e) // Ajoute toutes les classes css au bouton
     }); // Ajoute des class
     document.getElementById("objets").appendChild(button); // Ajoute le bouton dans la page
-
-    meshCount++;
-
+    grp_count++; //Incremente le nombre d'objets
   });
 }
